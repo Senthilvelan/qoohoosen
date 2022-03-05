caa7dff12789007df9733381425b1a22703af344
sen <velansms@gmail.com>
2022-03-05 22:53:04 +0530

    report

---
+++ app/src/main/java/com/qoohoosen/app/QooHooApplication.java
@@ -0,0 +1,24 @@
+package com.qoohoosen.app;
+
+import android.app.Application;
+
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+
+@ReportsCrashes(mailTo = "kssenthilvelan@gmail.com", customReportContent = {
+        ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME,
+        ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL,
+        ReportField.CUSTOM_DATA, ReportField.STACK_TRACE, ReportField.LOGCAT},
+        mode = ReportingInteractionMode.TOAST, resToastText = R.string.app_name)
+
+public class QooHooApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ACRA.init(this);
+    }
+}
